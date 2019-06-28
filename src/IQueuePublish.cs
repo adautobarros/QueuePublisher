8b554043134ce7906a4a9d9c0bb40c7347044826
Adauto <adauto.brrs@gmail.com>
2019-06-28 16:52:25 -0300

    commit inicial

---
+++ src/IQueuePublish.cs
@@ -0,0 +1,15 @@
+﻿namespace QueuePublisher
+{
+    public interface IQueuePublish
+    {
+        void Publish<T>(T Item, string exchangeName = "", string exchangeType = ExchangeType.Direct, string routingKey = "", string queueName = "");
+    }
+
+    public static class ExchangeType
+    {
+        public const string Direct = "direct";
+        public const string Fanout = "fanout";
+        public const string Headers = "headers";
+        public const string Topic = "topic";
+    }
+}
