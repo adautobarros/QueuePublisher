0548d62160950778e609f62b892db59cc1b01cff
Adauto <adauto.brrs@gmail.com>
2019-10-10 16:51:53 -0300

    adicionado RabbitInfoQueuePublushConfiguration

---
+++ src/RabbitConfiguration.cs
@@ -0,0 +1,37 @@
+﻿namespace QueuePublisher
+{
+    public class RabbitConfiguration
+    {
+        public string HostName { get; set; }
+        public int Port { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string ExchangeName { get; set; }
+        public string VirtualHost { get; set; }
+        public string ExchangeType { get; set; }
+        public string RoutingKey { get; set; }
+        public string QueueName { get; set; }
+        public int NumberOfWorkroles { get; set; }
+    }
+    public class RabbitHostConfiguration
+    {
+        public string HostName { get; set; }
+        public int Port { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string VirtualHost { get; set; }
+    }
+
+    public class RabbitInfoQueuePublushConfiguration
+    {
+        public string ExchangeName { get; set; }
+        public string ExchangeType { get; set; }
+        public string RoutingKey { get; set; }
+        public string QueueName { get; set; }
+    }
+
+    public class RabbitInfoQueueConfiguration : RabbitInfoQueuePublushConfiguration
+    {
+        public int NumberOfWorkroles { get; set; }
+    }
+}
