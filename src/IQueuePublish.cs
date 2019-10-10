5d33a42c35cc87d999f2734783c01f55ebebef25
Adauto <adauto.brrs@gmail.com>
2019-10-10 13:57:49 -0300

    adicionado classes auxiliares

--- src/IQueuePublish.cs
+++ src/IQueuePublish.cs
@@ -12,4 +12,34 @@
         public const string Headers = "headers";
         public const string Topic = "topic";
     }
+
+    public class RabbitConfiguration
+    {
+        public string HostName { get; set; }
+        public int Port { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string ExchangeName { get; set; }
+        public string VirtualHost { get; set; }
+        public string ExchangeType { get; set; }
+        public string RoutingKey { get; set; }
+        public string QueueName { get; set; }
+        public int NumberOfWorkroles { get; set; }
+    }
+    public class RabbitHostConfiguration
+    {
+        public string HostName { get; set; }
+        public int Port { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string VirtualHost { get; set; }
+    }
+    public class RabbitInfoQueueConfiguration
+    {
+        public string ExchangeName { get; set; }
+        public string ExchangeType { get; set; }
+        public string RoutingKey { get; set; }
+        public string QueueName { get; set; }
+        public int NumberOfWorkroles { get; set; }
+    }
 }
