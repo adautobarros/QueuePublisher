f39b788dcaf27169b7688f960a51c478590b3400
Adauto <adauto.brrs@gmail.com>
2019-11-18 10:52:25 -0300

    Adicionado herança na classe RabbitConfiguration de RabbitInfoQueueConfiguration

--- src/RabbitConfiguration.cs
+++ src/RabbitConfiguration.cs
@@ -1,18 +1,5 @@
 ﻿namespace QueuePublisher
 {
-    public class RabbitConfiguration
-    {
-        public string HostName { get; set; }
-        public int Port { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
-        public string ExchangeName { get; set; }
-        public string VirtualHost { get; set; }
-        public string ExchangeType { get; set; }
-        public string RoutingKey { get; set; }
-        public string QueueName { get; set; }
-        public int NumberOfWorkroles { get; set; }
-    }
     public class RabbitHostConfiguration
     {
         public string HostName { get; set; }
@@ -34,5 +21,14 @@
         public string QueueName { get; set; }
         public int NumberOfWorkroles { get; set; } = 1;
         public bool CreateDeadLetterQueue { get; set; } = false;
+    }
+
+    public class RabbitConfiguration : RabbitInfoQueueConfiguration
+    {
+        public string HostName { get; set; }
+        public int Port { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string VirtualHost { get; set; }
     }
 }
